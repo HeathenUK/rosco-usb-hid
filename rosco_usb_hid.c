e6327d3c171e7cabdfa22d2d3e186c0734ed338c
HeathenUK <65458864+HeathenUK@users.noreply.github.com>
2022-07-27 09:18:50 +0100

    Clarification of usage in demo code

--- rosco_usb_hid.c
+++ rosco_usb_hid.c
@@ -19,8 +19,14 @@ int main(int argc, char **argv) {
 
     while (true) {            
         
+        //Demo 1: fgets() equivalent. Will wait for either a \n terminated
+        //string or a max length and then return that string.
 
         printf("String: %s", ugets(&state, 50));
+        
+        //Demo 2: checkchar() equivalents. Will return true if input is
+        //is pending, false if not.
+
         // if (check_pad(&state)) {
         //     BUTTONS latest = read_pad(&state);
         //     printf("%u ", latest.dpad);
@@ -28,11 +34,13 @@ int main(int argc, char **argv) {
         //     printf("%u\n", latest.xpad);
         // }
 
-        // if (check_key(&state)) {
+        // if (check_key(&state)) printf("%c", read_key(&state));
 
-        //     printf("%c", read_key(&state));
+        //Demo 3: readchar() equivalent. Will block until input is pending
+        //and then return it.
+        
+        //  printf("%c", read_key(&state));
 
-        // }
 
     }
 }
